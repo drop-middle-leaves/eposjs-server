35802585089e3344d9e892c736dd68b213fc7d4e
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-02-07 12:40:28 +0000

    splitting git repos to make my life a little easier or whatever idk just wasting time

---
+++ database-init.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE jsepos;
+
+CREATE TABLE orders (
+    order_pk SERIAL PRIMARY KEY,
+    order_id VARCHAR(255) NULL,
+    isSettled BOOLEAN DEFAULT(FALSE) NOT NULL,
+    isSale BOOLEAN NOT NULL,
+    orderTime TIMESTAMP DEFAULT(CURRENT_TIMESTAMP) NOT NULL
+);
+
+CREATE TABLE order_item (
+    order_item_pk SERIAL PRIMARY KEY,
+    order_fk BIGINT NOT NULL,
+    product_fk BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    percentageModifier DECIMAL NULL,
+    FOREIGN KEY (order_fk) REFERENCES orders(order_pk),
+    FOREIGN KEY (product_fk) REFERENCES product(product_pk)
+);
+
+CREATE TABLE product (
+    product_pk SERIAL PRIMARY KEY,
+    EAN BIGINT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    vat_fk BIGINT NOT NULL,
+    stock INT NOT NULL,
+    FOREIGN KEY (vat_fk) REFERENCES vat(vat_pk)
+);
+
+CREATE TABLE vat (
+    vat_pk SERIAL PRIMARY KEY,
+    vatCode INTEGER NOT NULL,
+    vatRate DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE priceHistory (
+    priceHistory_pk SERIAL PRIMARY KEY,
+    product_fk BIGINT NOT NULL,
+    startDate TIMESTAMP NOT NULL,
+    endDate TIMESTAMP NULL,
+    net DECIMAL(10,2) NOT NULL,
+    gross DECIMAL(10,2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_fk) REFERENCES product(product_pk)
+);
---
+++ index.js
@@ -0,0 +1,232 @@
+// Pull in required dependencies, including the connection to the PostgreSQL database
+const express = require('express');
+const app = express();
+const pool = require('./db.js');
+require('dotenv').config();
+const { Client, Environment } = require("square");
+
+// Create a Square client
+const client = new Client({
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+  environment: Environment.Sandbox,
+});
+
+// Middleware
+app.use(express.json()); //req.body
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // Accept from any domain
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+// Routes
+
+// Create a new order
+app.post("/orderCreate", async (req, res, next) => {
+    try {
+        //retrieve the required data from the front end
+        const { isSale } = req.body;
+        const { orderItems } = req.body;
+        // create an array to store the data to be returned
+        let returnData = [];
+
+        // Create Payment Link in Square
+        try {
+            // Create unique key for Square (not used outside the api call)
+            const idempotencyKey = require('crypto').randomBytes(22).toString('hex');
+
+            // Create order json
+            let order = ({
+                "idempotencyKey": idempotencyKey,
+                "order": {
+                    "locationId": process.env.SQUARE_LOCATION_ID,
+                    "lineItems": [],
+                    "idempotencyKey": idempotencyKey
+                }
+            });
+
+            // Add line items to order
+            for (const key of orderItems) {
+                // Get product name and price from database
+                let name = await pool.query("SELECT description FROM product WHERE EAN = $1", [key.EAN]);
+                name = name.rows[0].description;
+                let price = await pool.query("SELECT priceHistory.gross FROM priceHistory JOIN product ON priceHistory.product_fk = product.product_pk WHERE ((product.EAN = $1) AND (((priceHistory.startDate <= CURRENT_TIMESTAMP) AND (priceHistory.endDate IS NULL)) OR ((priceHistory.startDate <= CURRENT_TIMESTAMP) AND (priceHistory.endDate > CURRENT_TIMESTAMP))));", [key.EAN]);
+                price = parseInt(price.rows[0].gross * 100);
+
+                // Create JSON for line item
+                const lineItems = ({
+                    "name": name,
+                    "quantity": String(key.Quantity),
+                    "basePriceMoney": {
+                        "amount": price,
+                        "currency": 'GBP'
+                    }
+                });
+
+                // Add JSON to Order
+                order.order.lineItems.push(lineItems);
+            }
+  
+            const response = await client.checkoutApi.createPaymentLink(order);
+
+            // Parses the JSON return value into something JS can use
+            const jsonResponse = JSON.parse(response.body);
+
+            // Sets the order_id and payment link to variables for use when committing to DB and returning to front end
+            var order_id = jsonResponse.payment_link.order_id;
+            var paymentLink = jsonResponse.payment_link.url;
+
+        } catch (ApiError) {
+            throw ApiError;
+        }
+        //create a new order in orders table
+        const newOrder = await pool.query("INSERT INTO orders (order_id, isSale) VALUES($1, $2) RETURNING *", [order_id, isSale]);
+        //get the order_pk of the new order
+        var order_pk = newOrder.rows[0].order_pk;
+
+        //Create a new order item in order_items table for each item in the orde
+        for (const key of orderItems) {
+            //Find the product_pk of the EAN for creating the order item
+            var product_fk = await pool.query("SELECT product_pk FROM product WHERE EAN = $1", [key.EAN]);
+            product_fk = product_fk.rows[0].product_pk;
+            //Create the new order item
+            await pool.query("INSERT INTO order_item (order_fk, product_fk, quantity, percentageModifier) VALUES($1, $2, $3, $4) RETURNING *", [order_pk, product_fk, key.Quantity, key.percentageModifier]);
+        }
+        
+        //Push the payment link and order_id to the return data
+        returnData.push({
+            "paymentLink": paymentLink,
+            "order_id": order_id
+        });
+
+        //return the data
+        res.json(returnData);
+    } catch (err) {
+        res.json(err.message);
+        console.error(err.message);
+    }
+});
+
+app.post("/search", async (req, res, next) => {
+    try {
+        // Retrieve the required data from the front end
+        let { search } = req.body;
+
+        // Allows for searching for products with various parts missing
+        // Split string
+        search = search.split(" ");
+        // Add % to each word
+        for (let i = 0; i < search.length; i++) {
+            search[i] = "%" + search[i] + "%"
+        }
+        // Join string
+        search = search.join(" ");
+        console.log(search);
+
+        // Create an array to store the data to be returned
+        let returnData = [];
+
+        // Search for products
+        const products = await pool.query("SELECT * FROM product WHERE description LIKE $1", [search]);
+        console.log(products.rows)
+        // Add products to return data
+        for (const key of products.rows) {
+            returnData.push({
+                "EAN": key.ean,
+                "Description": key.description
+            });
+        }
+
+        //return the data
+        res.json(returnData);
+    } catch (err) {
+        res.json(err.message);
+        console.error(err.message);
+    }
+});
+
+app.post("/getEanInfo", async (req, res, next) => {
+    try {
+        // Retrieve the required data from the front end
+        let { EAN } = req.body;
+
+        // Create an array to store the data to be returned
+        let returnData = [];
+
+        // Get product name and price from database
+        let name = await pool.query("SELECT description FROM product WHERE EAN = $1", [EAN]);
+        name = name.rows[0].description;
+        let price = await pool.query("SELECT priceHistory.gross FROM priceHistory JOIN product ON priceHistory.product_fk = product.product_pk WHERE ((product.EAN = $1) AND (((priceHistory.startDate <= CURRENT_TIMESTAMP) AND (priceHistory.endDate IS NULL)) OR ((priceHistory.startDate <= CURRENT_TIMESTAMP) AND (priceHistory.endDate > CURRENT_TIMESTAMP))));", [EAN]);
+        price = price.rows[0].gross;
+        let stock = await pool.query("SELECT stock FROM product WHERE EAN = $1", [EAN]);
+        stock = stock.rows[0].stock;
+
+        // Add products to return data
+        returnData.push({
+            "EAN": EAN,
+            "Description": name,
+            "Price": price,
+            "Stock": stock
+        });
+
+        // Return the data
+        res.json(returnData);
+    } catch (err) {
+        res.json(err.message);
+        console.error(err.message);
+    }
+});
+
+app.post("/getOrderInfo", async (req, res, next) => {
+    try {
+        // Retrieve the required data from the front end
+        let { order_id } = req.body;
+
+        // Get order info from database
+        let order = await pool.query("SELECT * FROM orders WHERE order_id = $1", [order_id]);
+        order = order.rows[0];
+        // Get order time from database
+        let orderTime = await pool.query("SELECT orderTime FROM orders WHERE order_id = $1", [order_id]);
+        orderTime = orderTime.rows[0].ordertime;
+
+        // Create order json
+        let returnData = ({
+            "isSale": order.issale,
+            "isSettled": order.issettled,
+            "orderItems": []
+        });
+        
+
+        // Get order items from database
+        let orderItems = await pool.query("SELECT * FROM order_item WHERE order_fk = $1", [order.order_pk]);
+        for (const key of orderItems.rows) {
+            // Get product name
+            let name = await pool.query("SELECT * FROM product WHERE product_pk = $1", [key.product_fk]);
+            name = name.rows[0].description;
+
+            // Get price from database at the time of the order
+            let price = await pool.query("SELECT priceHistory.gross, priceHistory.endDate FROM priceHistory JOIN product ON priceHistory.product_fk = product.product_pk WHERE ((product.product_pk = $1) AND (((priceHistory.startDate <= $2) AND (priceHistory.endDate IS NULL)) OR ((priceHistory.startDate <= $2) AND (priceHistory.endDate > $2))));", [key.product_fk, orderTime]);
+            price = price.rows[0].gross;
+
+            returnData.orderItems.push({
+                "product": name,
+                "quantity": key.quantity,
+                "price": price,
+                "percentageModifier": key.percentageModifier
+            }
+            );
+        }
+
+        // Return the data
+        res.json(returnData);
+    } catch (err) {
+        res.json(err.message);
+        console.error(err.message);
+    }
+});
+
+
+app.listen(5200, () => {
+    console.log('Server is running on port 5200');
+});
